ad9aa61ff6d5aa7f7fb3d917a6292f755a404f9e
kossouboly <tedji90@gmail.com>
2022-09-11 04:04:01 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - entry point of the project
+ *
+ *
+ *Return: return 0 everytime
+*/
+
+int main(void)
+{
+	char display[52];
+
+	strcpy(display, "\"Programming is like building a multilingual puzzle);
+	puts(display);
+	return (0);
+}
