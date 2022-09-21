6cd326ec33c5c6b12bb4ba90acdbd54fdda63591
kossouboly <tedji90@gmail.com>
2022-09-20 22:38:42 -0700

    task100__

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,9 +3,22 @@
 /**
  * _atoi - Entry point
  * @s: input
- * Return: Always 0 (Success)
+ *Return: The integer value of the comverted string.
 */
 int _atoi(char *s)
 {
-	return (*s);
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
 }
